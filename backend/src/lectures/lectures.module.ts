6f342f1a97e3120b75b00de8402d86fd5bbc1580
Patience Burani <buranipatiencepatricia@gmail.com>
2024-10-02 11:55:31 +0300

    updated the controller file

--- backend/src/lectures/lectures.module.ts
+++ backend/src/lectures/lectures.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LecturesService } from './lectures.service';
 import { LecturesController } from './lectures.controller';
+import { PrismaModule } from '../../prisma/prisma.module'; 
+// Ensure the correct import path
 
 @Module({
+  imports: [PrismaModule], // Import PrismaModule here
   providers: [LecturesService],
   controllers: [LecturesController],
-  exports: [LecturesService],
 })
 export class LecturesModule {}
--- backend/src/lectures/lectures.service.ts
+++ backend/src/lectures/lectures.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CreateLecturerSignUpDto } from './dto/create-lecturer.dto';
 
 @Injectable()
-export class LecturesService {}
+export class LecturesService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createLecturerSignUpDto: CreateLecturerSignUpDto) {
+    return this.prisma.lecturerSignUp.create({
+      data: {
+        first_name: createLecturerSignUpDto.first_name,
+        last_name: createLecturerSignUpDto.last_name,
+        email: createLecturerSignUpDto.email,
+        role: createLecturerSignUpDto.role,
+        password: createLecturerSignUpDto.password,
+      },
+    });
+  }
+}
