30cbbd5ab92b7271f40e9230e3d8a636fa327820
Phillip <phillipbaguma1@gmail.com>
2024-10-02 15:15:27 +0300

    add update-user.dto

--- backend/src/lectures/lectures.controller.ts
+++ backend/src/lectures/lectures.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Param, Patch } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { LecturesService } from './lectures.service';
 
 @Controller('lectures')
-export class LecturesController {}
+export class LecturesController {
+    constructor(private readonly lecturesService: LecturesService) {}
+}
