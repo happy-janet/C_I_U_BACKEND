0b9df3c7b47424a6ce71b7df6a6c3ba8670afc96
akullu winnifred <akulluwinnifred1@gmail.com>
2024-10-02 14:43:51 +0300

    creating view and delete feature

--- backend/src/lectures/lectures.controller.ts
+++ backend/src/lectures/lectures.controller.ts
@@ -1,13 +1,36 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Delete } from '@nestjs/common';
 import { CreateLecturerSignUpDto } from './dto/create-lecturer.dto';
-import { LecturesService } from './lectures.service'; // Import the service
+import { LecturesService } from './lectures.service'; 
 
 @Controller('lectures')
 export class LecturesController {
-  constructor(private readonly lecturesService: LecturesService) {} // Inject the service
+  constructor(private readonly lecturesService: LecturesService) {} 
 
   @Post()
   async create(@Body() createLecturerSignUpDto: CreateLecturerSignUpDto) {
-    return await this.lecturesService.create(createLecturerSignUpDto); // Call the service method
+    return await this.lecturesService.create(createLecturerSignUpDto); 
   }
+
+
+//   @Get()
+//   async getAllBooks(): Promise<Book[]> {
+//       return this.lecturesService.findAll();
+//   }
+@Get()
+async findAll() {
+  return this.lecturesService.findAll(); 
+}
+
+// Get a single lecturer by ID
+@Get(':id')
+async findOne(@Param('id') id: string) {
+  return this.lecturesService.findOne(+id); 
+}
+
+
+// Delete a lecturer by ID
+@Delete(':id')
+async delete(@Param('id') id: string) {
+  return this.lecturesService.delete(+id); // Call the service method to delete a lecturer
+}
 }
