242d104790aad83e1a1a93e7d12d1c142366612b
katusabe scovia <scoviak588@gmail.com>
2024-10-03 10:14:42 +0300

    added manage admindashboard

--- backend/src/students/students.module.ts
+++ backend/src/students/students.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
+import { PrismaService } from '../../prisma/prisma.service'; 
 
 @Module({
-  providers: [StudentsService],
   controllers: [StudentsController],
-  exports: [StudentsService],
+  providers: [StudentsService, PrismaService], 
 })
 export class StudentsModule {}
