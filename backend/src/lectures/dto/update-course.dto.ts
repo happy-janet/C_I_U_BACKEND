be4ca9f715e860e6cae6498c846c6d050e862edd
Patience Burani <buranipatiencepatricia@gmail.com>
2024-10-10 10:35:32 +0300

    added the CRUD

---
+++ backend/src/lectures/dto/update-course.dto.ts
@@ -0,0 +1,18 @@
+// src/courses/dto/create-course.dto.ts
+import { IsArray, IsString } from 'class-validator';
+
+export class UpdateCourseDto {
+  @IsString()
+  facultyName: string;
+
+  @IsString()
+  courseName: string;
+
+  @IsArray()
+  @IsString({ each: true }) // Ensures each element in the array is a string
+  courseUnits: string[];
+
+  courseUnitCode: string;
+}
+
+
