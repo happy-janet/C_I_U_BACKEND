f331fa465d9d43488bf5886dac9d304fc6c900ae
akullu winnifred <akulluwinnifred1@gmail.com>
2024-10-10 10:10:41 +0300

    debugging

--- backend/src/lectures/dto/create-course.dto.ts
+++ backend/src/lectures/dto/create-course.dto.ts
@@ -1,14 +1,6 @@
-// src/courses/dto/create-course.dto.ts
-import { IsArray, IsString } from 'class-validator';
-
 export class CreateCourseDto {
-  @IsString()
   facultyName: string;
-
-  @IsString()
   courseName: string;
-
-  @IsArray()
-  @IsString({ each: true }) // Ensures each element in the array is a string
-  courseUnits: string[];
+  courseUnits: string[];  // Matches the schema field
+  courseUnitCode: string; // Matches the schema field
 }
