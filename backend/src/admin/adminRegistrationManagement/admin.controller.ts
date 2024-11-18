05a9d6e446bbedb709f36aa505b0a37c08bfffa8
akullu winnifred <akulluwinnifred1@gmail.com>
2024-11-18 18:55:29 +0300

    Renaming files and organizing them

---
+++ backend/src/admin/adminRegistrationManagement/admin.controller.ts
@@ -0,0 +1,54 @@
+import { Controller, Post, Body, Patch, Param, UseGuards,Get, Delete,Req ,BadRequestException
+} from '@nestjs/common';
+import { CreateAdminSignUpDto } from './adminRegistrationManagementDto/create-admin.dto';
+import { AdminService } from './admin.service'; 
+import { UpdateUserDto } from '../../lecturer/lecturerManagement/lecturerManagementDto/update-lecturer.dto';
+import { JwtAuthGuard } from '../adminAuth/JwtAuthGuard';
+@Controller('adminReg')
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {} 
+
+  @Post()
+  async create(@Body() createAdminSignUpDto: CreateAdminSignUpDto) {
+    return await this.adminService.create(createAdminSignUpDto); 
+  }
+
+  // Update admin by ID
+  @Patch(':id')
+  async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.adminService.updateUser(id, updateUserDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.adminService.findAll(); 
+  }
+
+  // Get a single admin by ID
+  @Get(':id')
+async findOne(@Param('id') id: string) {
+  const numericId = +id;
+
+  if (isNaN(numericId)) {
+    throw new BadRequestException('Invalid ID: ID must be a number');
+  }
+
+  console.log('Received ID:', numericId); 
+  return this.adminService.findOne(numericId);
+}
+
+
+
+  // Delete an admin by ID
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.adminService.delete(+id); 
+  }
+
+
+  @Get('profile/:id')
+async getProfile(@Param('id') id: string) {
+  return this.adminService.getProfile(+id); 
+}
+
+}
--- backend/src/lectures/token-generator.ts
+++
@@ -1,6 +0,0 @@
-
-
-export function generateNumericToken(length: number): string {
-    const multiplier = Math.pow(10, length - 1); // Ensures token has required number of digits
-    return Math.floor(multiplier + Math.random() * (9 * multiplier)).toString();
-}
