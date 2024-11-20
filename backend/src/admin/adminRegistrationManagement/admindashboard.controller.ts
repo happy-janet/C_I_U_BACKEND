337c593daa25f6346d942cd81d34f3862ed7075b
Burani0 <buranipatiencepatricia@gmail.com>
2024-11-20 10:36:44 +0300

    editted the .env file

--- backend/src/admin/adminRegistrationManagement/admindashboard.controller.ts
+++ backend/src/admin/adminRegistrationManagement/admindashboard.controller.ts
@@ -9,8 +9,4 @@ export class DashboardController {
     return { message: 'Welcome to the Dashboard' }; 
   }
 }
-<<<<<<< HEAD:backend/src/admin/admindashboard.controller.ts
-// To the lecturer dashboard
 
-=======
->>>>>>> 543247f3111897e6feadd6d23c00c30bd3202db2:backend/src/admin/adminRegistrationManagement/admindashboard.controller.ts
